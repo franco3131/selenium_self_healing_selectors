861e5c24627ef3ef95be035ef9d607a247ccf764
David Franco <franco3131@yahoo.com>
2025-09-22 23:15:47 -0700

    Update RunnerFile.java

--- src/test/java/com/wikipedia/runners/RunnerFile.java
+++ src/test/java/com/wikipedia/runners/RunnerFile.java
@@ -1,19 +1,17 @@
 package com.wikipedia.runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
-    glue = { "com.wikipedia.steps", "com.wikipedia.support" },
+    glue = {"com.wikipedia.steps", "com.wikipedia.support"},
     plugin = {
         "pretty",
-        "html:target/cucumber-report.html",
+        "html:target/cucumber-reports/cucumber.html",
         "json:target/cucumber.json",
         "junit:target/cucumber.xml"
     },
     monochrome = true
 )
-public class RunnerFile { }
+public class RunnerTest extends AbstractTestNGCucumberTests { }
