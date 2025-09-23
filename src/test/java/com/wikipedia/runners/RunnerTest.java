38f76ddffc13ff6441890a69d2ca34ae6310bf82
David Franco <franco3131@yahoo.com>
2025-09-22 23:41:37 -0700

    Update RunnerTest.java

--- src/test/java/com/wikipedia/runners/RunnerTest.java
+++ src/test/java/com/wikipedia/runners/RunnerTest.java
@@ -1,1 +1,17 @@
+package com.wikipedia.runners;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"com.wikipedia.steps", "com.wikipedia.support"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber.json",
+        "junit:target/cucumber.xml"
+    },
+    monochrome = true
+)
+public class RunnerTest extends AbstractTestNGCucumberTests { }
