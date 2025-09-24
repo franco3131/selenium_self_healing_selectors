113811ba4b9bffaf4a9ef510a69f4d8608572dbc
David Franco <franco3131@yahoo.com>
2025-09-23 23:54:21 -0700

    Update BaseTest.java

--- src/test/java/com/wikipedia/support/BaseTest.java
+++ src/test/java/com/wikipedia/support/BaseTest.java
@@ -5,6 +5,9 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class BaseTest {
 
@@ -12,9 +15,26 @@ public class BaseTest {
 
     @Before(order = 0)
     public void startBrowserSession() {
-        WebDriver standardBrowserDriver = new ChromeDriver();
-        SelfHealingDriver healingBrowserDriver = SelfHealingDriver.create(standardBrowserDriver);
-        threadLocalBrowser.set(healingBrowserDriver);
+    ChromeOptions options = new ChromeOptions();
+
+    // Unique profile dir (important for GitHub Actions/Linux)
+    Path tmpProfile = Files.createTempDirectory("gha-chrome-profile-");
+    options.addArguments("--user-data-dir=" + tmpProfile.toAbsolutePath().toString());
+
+    // Headless & CI-safe flags
+    options.addArguments("--headless=new");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--window-size=1920,1080");
+
+    // optional: avoid remote origin issues
+    options.addArguments("--remote-allow-origins=*");
+
+    WebDriver standardBrowserDriver = new ChromeDriver(options);
+    SelfHealingDriver healingBrowserDriver = SelfHealingDriver.create(standardBrowserDriver);
+
+    threadLocalBrowser.set(healingBrowserDriver);
     }
 
     @After(order = 0)
