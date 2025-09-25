ac5619bc216f40753046d644274b83198f53e57a
David Franco <franco3131@yahoo.com>
2025-09-24 23:24:03 -0700

    Update BasePage.java

--- src/test/java/com/wikipedia/pages/BasePage.java
+++ src/test/java/com/wikipedia/pages/BasePage.java
@@ -13,7 +13,7 @@ public abstract class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     protected WebElement waitForVisible(By locator) {
