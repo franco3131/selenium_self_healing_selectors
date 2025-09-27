1d6b153713cbf1e52ffd39f8053753cef59117d9
David Franco <franco3131@yahoo.com>
2025-09-26 23:38:11 -0700

    Update BaseTest.java

--- src/test/java/com/wikipedia/support/BaseTest.java
+++ src/test/java/com/wikipedia/support/BaseTest.java
@@ -2,6 +2,8 @@ package com.wikipedia.support;
 
 import com.epam.healenium.SelfHealingDriver;
 import io.cucumber.java.After;
+import io.github.shelennium.healenium.events.HealingEvent;
+import io.github.shelennium.healenium.events.HealingEventListener;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +17,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.OutputType;
+import java.nio.file.StandardCopyOption;
 
 public class BaseTest {
 
@@ -32,6 +35,7 @@ public void startBrowserSession() throws IOException {
      WebDriver standard = new ChromeDriver(options);
     WebDriver healing = SelfHealingDriver.create(standard);
     threadLocalBrowser.set(healing);
+    
 }
 
     @After(order = 0)
@@ -64,5 +68,21 @@ public void startBrowserSession() throws IOException {
             System.err.println("Could not save screenshot: " + e.getMessage());
         }
     }
+    @AfterMethod(alwaysRun = true)
+public void takeScreenshotOnFailure(ITestResult result) {
+    WebDriver driver = getCurrentBrowser();
+    if (driver != null && !result.isSuccess()) {
+        try {
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String filename = "screenshots/fail-" + result.getName() + "-" + System.currentTimeMillis() + ".png";
+            File dest = new File(filename);
+            dest.getParentFile().mkdirs(); // make sure dir exists
+            Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("[TESTNG] Screenshot saved: " + dest.getAbsolutePath());
+        } catch (Exception e) {
+            System.err.println("[TESTNG] Could not capture screenshot: " + e.getMessage());
+        }
+    }
+}
     
 }
