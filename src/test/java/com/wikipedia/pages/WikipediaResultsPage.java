e2eb44f79d43612c134e3b759beec20e2cf38760
David Franco <franco3131@yahoo.com>
2025-10-03 00:18:50 -0700

    Update WikipediaResultsPage.java

--- src/test/java/com/wikipedia/pages/WikipediaResultsPage.java
+++ src/test/java/com/wikipedia/pages/WikipediaResultsPage.java
@@ -3,6 +3,8 @@ package com.wikipedia.pages;
 import com.wikipedia.pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
 
 public class WikipediaResultsPage extends BasePage {
     private final By firstHeading = By.id("firstHeading");
@@ -11,6 +13,7 @@ public class WikipediaResultsPage extends BasePage {
         super(driver);
     }
     public Boolean doesSearchedTextDisplayTopResults(int rows,String word) {
+        try{
     	String sentence = "";
     	for(int i=1;i<=rows;i++) {
     		System.out.println("body > div > a > div > a:nth-child("+i+") > div>p>b:nth-child(1)");
@@ -23,6 +26,20 @@ public class WikipediaResultsPage extends BasePage {
     		}
     	}
     	return true;
+        }catch(Exception e){
+    ((JavascriptExecutor) driver).executeScript("document.activeElement.blur();");
+    	String sentence = "";
+    	for(int i=1;i<=rows;i++) {
+    		System.out.println("body > div > a > div > a:nth-child("+i+") > div>p>b:nth-child(1)");
+    		waitForVisible(By.cssSelector("body > div > a > div > a:nth-child("+i+") > div"));
+    		sentence = driver.findElement(By.cssSelector("body > div > a > div > a:nth-child("+i+") > div>p>b:nth-child(1)")).getText();
+    		System.out.println(sentence);
+            System.out.println(word);
+    		if(!sentence.toLowerCase().contains(word)) {
+    			return false;
+    		}
+
+        }
     }
     
 }
