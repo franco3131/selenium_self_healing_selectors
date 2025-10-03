848f8290e8838f0521a0051c52c2391c6b0c85b5
David Franco <franco3131@yahoo.com>
2025-10-03 00:40:10 -0700

    Update WikipediaHomePage.java

--- src/test/java/com/wikipedia/pages/WikipediaHomePage.java
+++ src/test/java/com/wikipedia/pages/WikipediaHomePage.java
@@ -2,6 +2,7 @@ package com.wikipedia.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class WikipediaHomePage extends BasePage {
 
